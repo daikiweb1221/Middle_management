4eac79d08bc120012f0abad7fdd5b0410a8428fd
daikiweb1221 <daiki.web1221@gmail.com>
2022-10-24 09:42:36 +0900

    Add: vue-router導入,セットアップ

--- api/app/javascript/packs/hello_vue.js
+++ api/app/javascript/packs/hello_vue.js
@@ -7,11 +7,13 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import router from '../router'
 
 Vue.config.productionTip = false
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    router,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
