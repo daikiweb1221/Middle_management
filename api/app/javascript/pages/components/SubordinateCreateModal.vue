b986cc25533bb8da805eb0a3146d3eaa198bf1ca
daikiweb1221 <daiki.web1221@gmail.com>
2022-11-07 05:55:01 +0900

    Add: バリデーション追加

--- api/app/javascript/pages/components/SubordinateCreateModal.vue
+++ api/app/javascript/pages/components/SubordinateCreateModal.vue
@@ -4,37 +4,52 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-body">
-            <div class="form-group">
-              <label for="name">名前</label>
-              <input
-                type="text"
-                class="form-control"
-                id="name"
-                v-model="subordinate.name"
-              >
-            </div>
-            <div class="form-group">
-              <label for="email">メールアドレス</label>
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                v-model="subordinate.email"
-              >
-            </div>
-            <div class="form-group">
-              <label for="birthday">誕生日</label>
-              <input
-                type="date"
-                class="form-control"
-                id="birthday"
-                v-model="subordinate.birthday"
-              >
-            </div>
-            <div class="d-flex justify-content-between">
-              <button class="btn btn-success" @click="handleCreateSubordinate">追加</button>
-              <button class="btn btn-secondary" @click="handleCloseModal">閉じる</button>
-            </div>
+            <ValidationObserver v-slot="{ handleSubmit }">
+              <div class="form-group">
+                <ValidationProvider v-slot="{ errors }" rules="required|max:20">
+                  <label for="name">名前</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    v-model="subordinate.name"
+                  />
+                  <span class="text-danger">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+              <div class="form-group">
+                <ValidationProvider v-slot="{ errors }" rules="email">
+                  <label for="email">メールアドレス</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    v-model="subordinate.email"
+                  />
+                  <span class="text-danger">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+              <div class="form-group">
+                <label for="birthday">誕生日</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="birthday"
+                  v-model="subordinate.birthday"
+                />
+              </div>
+              <div class="d-flex justify-content-between">
+                <button
+                  class="btn btn-success"
+                  @click="handleSubmit(handleCreateSubordinate)"
+                >
+                  追加
+                </button>
+                <button class="btn btn-secondary" @click="handleCloseModal">
+                  閉じる
+                </button>
+              </div>
+            </ValidationObserver>
           </div>
         </div>
       </div>
@@ -49,21 +64,21 @@ export default {
   data() {
     return {
       subordinate: {
-        name: '',
-        email: '',
-        birthday: ''
-      }
-    }
+        name: "",
+        email: "",
+        birthday: "",
+      },
+    };
   },
   methods: {
     handleCloseModal() {
-      this.$emit('close-modal')
+      this.$emit("close-modal");
     },
     handleCreateSubordinate() {
-      this.$emit('create-subordinate', this.subordinate)
-    }
-  }
-}
+      this.$emit("create-subordinate", this.subordinate);
+    },
+  },
+};
 </script>
 
 <style scoped>
--- api/app/javascript/pages/login/index.vue
+++ api/app/javascript/pages/login/index.vue
@@ -1,40 +1,46 @@
 <template>
   <div id="login-form" class="container w-50 text-center">
-    <div class="h3 mb-3">
-      ログイン
-    </div>
-    <div class="form-group text-left">
-      <label for="email">メールアドレス</label>
-      <input
-        id="email"
-        v-model="user.email"
-        type="email"
-        class="form-control"
-        placeholder="test@example.com"
+    <div class="h3 mb-3">ログイン</div>
+    <ValidationObserver v-slot="{ handleSubmit }">
+      <div class="form-group text-left">
+        <ValidationProvider v-slot="{ errors }" rules="required|email">
+          <label for="email">メールアドレス</label>
+          <input
+            id="email"
+            v-model="user.email"
+            type="email"
+            class="form-control"
+            placeholder="test@example.com"
+          />
+          <span class="text-danger">{{ errors[0] }}</span>
+        </ValidationProvider>
+      </div>
+      <div class="form-group text-left">
+        <ValidationProvider v-slot="{ errors }" rules="required|min:3">
+          <label for="password">パスワード</label>
+          <input
+            id="password"
+            v-model="user.password"
+            type="password"
+            class="form-control"
+            placeholder="password"
+          />
+          <span class="text-danger">{{ errors[0] }}</span>
+        </ValidationProvider>
+      </div>
+      <button
+        type="submit"
+        class="btn btn-primary"
+        @click="handleSubmit(login)"
       >
-    </div>
-    <div class="form-group text-left">
-      <label for="password">パスワード</label>
-      <input
-        id="password"
-        v-model="user.password"
-        type="password"
-        class="form-control"
-        placeholder="password"
-      >
-    </div>
-    <button
-      type="submit"
-      class="btn btn-primary"
-      @click="login"
-    >
-      ログイン
-    </button>
+        ログイン
+      </button>
+    </ValidationObserver>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex"
+import { mapActions } from "vuex";
 
 export default {
   name: "LoginIndex",
@@ -43,25 +49,21 @@ export default {
       user: {
         email: "",
         password: "",
-      }
-    }
+      },
+    };
   },
   methods: {
-    ...mapActions("users", [
-      "loginUser",
-      "fetchUser",
-    ]),
+    ...mapActions("users", ["loginUser", "fetchUser"]),
     async login() {
       try {
         await this.loginUser(this.user);
-        this.$router.push({ name: 'SubordinateIndex' })
+        this.$router.push({ name: "SubordinateIndex" });
       } catch (error) {
         console.log(error);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
