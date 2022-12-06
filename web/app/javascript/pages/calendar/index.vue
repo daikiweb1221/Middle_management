f03a463417998bea83e431db3aba1b5ade21d981
daikiweb1221 <daiki.web1221@gmail.com>
2022-12-06 17:07:26 +0900

    Add: 予定詳細ダイアログ追加

--- web/app/javascript/pages/calendar/index.vue
+++ web/app/javascript/pages/calendar/index.vue
@@ -1,33 +1,69 @@
 <template>
   <div>
-    <h1>Calendar</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </li>
-    </ul>
-    <button type="submit" @click="fetchEvents()">fetchEvents</button>
-
-    <CalendarDetail />
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="81vh">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="
+          (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+        "
+        @click:event="showEvent"
+      ></v-calendar>
+    </v-sheet>
+    <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
+      <EventDetailDialog v-if="event !== null" />
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import { format } from "date-fns";
 import { mapGetters, mapActions } from "vuex";
-import CalendarDetail from "../components/CalendarDetail";
+  import EventDetailDialog from '../components/EventDetailDialog';
+
 export default {
   name: "CalendarIndex",
 
   components: {
-    CalendarDetail,
-  },
+      EventDetailDialog,
+    },
+
+  data: () => ({
+    value: format(new Date(), "yyyy/MM/dd"),
+  }),
 
   computed: {
-    ...mapGetters("events", ["events"]),
+    ...mapGetters("events", ["events", "event"]),
+    title() {
+      return format(new Date(this.value), "yyyy年 M月");
+    },
   },
 
   methods: {
-    ...mapActions("events", ["fetchEvents"]),
+    ...mapActions("events", ["fetchEvents", "setEvent"]),
+    setToday() {
+      this.value = format(new Date(), "yyyy/MM/dd");
+    },
+    showEvent({ event }) {
+      this.setEvent(event);
+    },
+    closeDialog() {
+      this.setEvent(null);
+    },
   },
 };
 </script>
--- web/app/javascript/store/modules/events.js
+++ web/app/javascript/store/modules/events.js
@@ -2,20 +2,40 @@ import axios from "../../plugins/axios";
 
 const state = {
   events: [],
+  event: null,
 };
 
 const getters = {
-  events: (state) => state.events,
+  events: (state) =>
+    state.events.map((event) => {
+      return {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
+      };
+    }),
+  event: (state) =>
+    state.event
+      ? {
+          ...state.event,
+          start: new Date(state.event.start),
+          end: new Date(state.event.end),
+        }
+      : null,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  setEvent: (state, event) => (state.event = event),
 };
 
 const actions = {
   async fetchEvents({ commit }) {
     const res = await axios.get("events");
     commit("setEvents", res.data);
+  },
+  setEvent({ commit }, event) {
+    commit("setEvent", event);
   },
 };
 
