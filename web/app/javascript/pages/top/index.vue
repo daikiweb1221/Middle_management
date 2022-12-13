1e0ee455319f0b8e8fd0a950d4eb4725df6295e4
daikiweb1221 <daiki.web1221@gmail.com>
2022-12-13 14:37:56 +0900

    Add: トップページ実装完了

--- web/app/javascript/pages/top/index.vue
+++ web/app/javascript/pages/top/index.vue
@@ -6,78 +6,173 @@
           >ログイン</v-btn
         >
       </div>
-      <div class="main">
-        <div class="main__cont">
-          <div class="main__left">
-            <div class="main__title-cont">
-              <h1 class="main__title">Middle<br />Management</h1>
-            </div>
-            <div class="main__desc-cont">
-              <p class="main__desc">人に信頼される</p>
-              <p class="main__desc two">人に動いてもらえる</p>
-              <p class="main__desc three">そんな"理想とする自分"へ</p>
-            </div>
-            <div class="main__btn-wrap">
-              <v-btn
-                class="main-btn main_btn__one"
-                :to="{ name: 'RegisterIndex' }"
-                >さっそく始める</v-btn
-              >
-              <v-btn
-                class="main-btn main_btn__two"
-                :to="{ name: 'IdealBossChecksNotLogin' }"
-                >上司力をチェックする</v-btn
-              >
-            </div>
+    </div>
+    <section class="main l-main">
+      <div class="main__cont">
+        <div class="main__left">
+          <div class="main__title-cont">
+            <h1 class="main__title">Middle<br />Management</h1>
           </div>
-          <div class="main__right">
-            <img
-              src="../../../assets/images/top.png"
-              alt="メインビジュアル"
-              class="main__visual"
-            />
+          <div class="main__desc-cont">
+            <p class="main__desc">人に信頼される</p>
+            <p class="main__desc two">人に動いてもらえる</p>
+            <p class="main__desc three">そんな"理想とする自分"へ</p>
+          </div>
+          <div class="main__btn-wrap">
+            <v-btn
+              class="main-btn main_btn__one"
+              :to="{ name: 'RegisterIndex' }"
+              >さっそく始める</v-btn
+            >
+            <v-btn
+              class="main-btn main_btn__two"
+              :to="{ name: 'IdealBossChecksNotLogin' }"
+              >上司力をチェックする</v-btn
+            >
           </div>
         </div>
+        <div class="main__right">
+          <img
+            src="../../../assets/images/top.png"
+            alt="メインビジュアル"
+            class="main__visual"
+          />
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="service-overview l-service-overview">
+      <div class="service-overview__inner inner">
+        <div class="service-overview__title-cont sub-title-cont">
+          <h2 class="service-overview__title sub-title">Service overview</h2>
+          <p class="service-overview__title-j sub-title-j">- サービス概要 -</p>
+        </div>
+        <div class="service-overview__desc-cont">
+          <p class="service-overview__desc service-overview__desc-one">
+            部下に信頼されたい 部下の心を動かしたいなど
+          </p>
+          <p class="service-overview__desc service-overview__desc-two">
+            お悩みを持つ中間管理職のみなさまへ
+          </p>
+          <p class="service-overview__desc service-overview__desc-three">
+            自身の日々の立ち振る舞いや行動を見える化し
+          </p>
+          <p class="service-overview__desc service-overview__desc-four">
+            理想とする自分へと導くサービスです
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="contents l-contents">
+      <div class="contents__inner inner">
+        <div class="contents__title-cont sub-title-cont">
+          <h2 class="contents__title sub-title">Contents</h2>
+          <p class="contents__title-j sub-title-j">- サービス内容 -</p>
+        </div>
+        <div class="contents__desc-cont">
+          <div class="contents__block">
+            <div class="contents__block-left">
+              <h3 class="contents__block-title">メンバーを登録しよう</h3>
+              <div class="contents__block-desc-cont">
+                <p class="contents__block-desc">
+                  まずは管理したい部下を登録してみましょう<br />そうすることでコミュニケーションの見える化や部下ごとに褒めポイントを追加できるようになります
+                </p>
+              </div>
+            </div>
+            <div class="contents__block-right">
+              <div class="contents__block-img-wrap contents-img__wrap">
+                <img
+                  src="../../../assets/images/contents1.png"
+                  alt="メンバー登録"
+                  class="contents-img"
+                  style="width: 100%"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contents__desc-cont">
+          <div class="contents__block">
+            <div class="contents__block-right">
+              <div class="contents__block-img-wrap contents-img__wrap">
+                <img
+                  src="../../../assets/images/contents2.png"
+                  alt="コミュニケーションの見える化"
+                  class="contents-img"
+                  style="width: 70%"
+                />
+              </div>
+            </div>
+            <div class="contents__block-left">
+              <h3 class="contents__block-title">
+                コミュニケーションを<br />見える化しよう
+              </h3>
+              <div class="contents__block-desc-cont">
+                <p class="contents__block-desc">
+                  部下1人1人としっかりコミュニケーション取れていますか？<br />ハートマークをチェックしてコミュニケーションを見える化しましょう！
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contents__desc-cont">
+          <div class="contents__block">
+            <div class="contents__block-left">
+              <h3 class="contents__block-title">褒めポイントを集めよう</h3>
+              <div class="contents__block-desc-cont">
+                <p class="contents__block-desc">
+                  部下ごとに褒めポイントを追加しましょう<br />部下をしっかりと観察して褒めポイントがあればすかさず追加！みんなの前で褒めましょう！
+                </p>
+              </div>
+            </div>
+            <div class="contents__block-right">
+              <div class="contents__block-img-wrap contents-img__wrap">
+                <img
+                  src="../../../assets/images/contents3.png"
+                  alt="褒めポイント"
+                  class="contents-img"
+                  style="width: 70%"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contents__desc-cont">
+          <div class="contents__block">
+            <div class="contents__block-right">
+              <div class="contents__block-img-wrap contents-img__wrap">
+                <img
+                  src="../../../assets/images/contents4.png"
+                  alt="立ち振る舞いチェッっく"
+                  class="contents-img"
+                  style="width: 70%"
+                />
+              </div>
+            </div>
+            <div class="contents__block-left">
+              <h3 class="contents__block-title">
+                立ち振る舞いをチェックしよう
+              </h3>
+              <div class="contents__block-desc-cont">
+                <p class="contents__block-desc">
+                  ご自身の日常の姿こそが全ての土台となります<br />立ち振る舞いチェックを行うことで日々の行動を定期的に振り返りましょう
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="contents-btn-cont">
+          <v-btn class="main-btn main_btn__one" :to="{ name: 'RegisterIndex' }"
+            >さっそく始める</v-btn
+          >
+        </div>
+      </div>
+    </section>
   </div>
-  <!-- <div class="mv" :style="{ 'background-image': 'url(' + assetsImage + ')' }">
-    <div class="px-6 pt-15">
-      <p style="color: white" class="desc-title font-weight-bold">
-        "人に信頼される"、"人に動いてもらえる"そんな上司とは...
-      </p>
-      <p style="color: white" class="desc-title font-weight-bold">
-        Middle Managementはあなたを「<span
-          class="ideal font-weight-bold"
-          style="color: #d3b102"
-        >
-          理想とする自分 </span
-        >」へと導きます
-      </p>
-    </div>
-    <div class="mt-cont">
-      <h1 class="mt-title font-weight-bold">Middle Management</h1>
-      <div class="mt-btn-wrap mt-15">
-        <v-btn
-          class="ideal-btn font-weight-bold py-1"
-          :to="{ name: 'IdealBossChecksNotLogin' }"
-          >今すぐ自分の上司力をチェックする</v-btn
-        >
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
-// import AssetsImage from "../../../assets/images/middlemanagement-top.jpg";
-
 export default {
   name: "TopIndex",
-  // data() {
-  //   return {
-  //     assetsImage: AssetsImage,
-  //   };
-  // },
 };
 </script>
 
@@ -87,13 +182,35 @@ export default {
   padding-right: 4rem;
   padding-top: 1.5625rem;
 }
-.v-btn__content {
-}
 .v-btn {
   font-size: 1.5625rem !important;
   font-weight: 700 !important;
   color: #094067 !important;
 }
+
+.inner {
+  padding-left: 1.25rem !important;
+  padding-right: 1.25rem !important;
+}
+
+.sub-title-cont {
+  text-align: center;
+  padding-top: 4rem;
+}
+
+.sub-title {
+  font-size: 4rem;
+}
+.sub-title-j {
+  font-size: 1.3rem;
+  color: #5f6c7b !important;
+}
+
+/* ------------------メインビジュアル------------------ */
+.l-main {
+  margin-bottom: 9.875rem;
+}
+
 .main__cont {
   position: relative;
   max-width: 1440px;
@@ -161,6 +278,79 @@ export default {
   margin-left: 1.25rem;
   background-color: #ef4565 !important;
 }
+
+/* ------------------メインビジュアル------------------ */
+
+/* ------------------サービス概要------------------ */
+
+.l-service-overview {
+  padding-bottom: 8.5625rem;
+}
+
+.service-overview {
+  background-color: #d8eefe;
+}
+
+.service-overview__desc-cont {
+  text-align: center;
+  margin-top: 4.4375rem;
+}
+.service-overview__desc {
+  font-size: 1.7rem;
+  color: #5f6c7b !important;
+}
+
+/* ------------------サービス概要------------------ */
+
+/* ------------------サービス内容------------------ */
+
+.l-contents {
+  margin-bottom: 8.125rem;
+}
+.contents__block {
+  display: flex;
+  max-width: 1163px;
+  margin: auto;
+}
+
+.contents__desc-cont {
+  margin-top: 4.375rem;
+}
+
+.contents__block-left {
+  width: 51%;
+  margin-top: 3.125rem;
+}
+.contents__block-right {
+  width: 49%;
+}
+
+.contents-img__wrap {
+  width: 100%;
+}
+
+contents-img {
+  width: 100%;
+}
+
+.contents__block-title {
+  font-size: 2.1875rem;
+}
+
+.contents__block-desc-cont {
+  margin-top: 1.5625rem;
+}
+
+.contents__block-desc {
+  font-size: 1.3rem;
+  color: #5f6c7b !important;
+  line-height: 1.8 !important;
+}
+.contents-btn-cont {
+  text-align: center;
+  margin-top: 5rem;
+}
+
 /* .mv {
   position: relative;
   width: 100%;
