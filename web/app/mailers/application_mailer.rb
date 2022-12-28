82b1ee3df6228de1d0e8ca64d2b4a51786825c52
daikiweb1221 <daiki.web1221@gmail.com>
2022-12-28 18:44:48 +0900

    Fix: production.rb修正

--- web/app/mailers/application_mailer.rb
+++ web/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'middlemanagement.app@gmail.com'
+  default from: Rails.application.credentials.gmail[:user_name]
   layout 'mailer'
 end
