c2c7d9ce5f063b482346d9e1db4daf85e289466a
daikiweb1221 <daiki.web1221@gmail.com>
2022-12-28 21:44:14 +0900

    Fix: production.rbの修正

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'middlemanagement.app.magia.runteq.jp' }
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
