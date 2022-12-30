c915d900d4e86ad09c2c05bf3040440eceda87aa
daikiweb1221 <daiki.web1221@gmail.com>
2022-12-31 07:23:44 +0900

    Fix: user_mailer.rbのsite.top_urlを本番環境へ変更

--- web/app/mailers/user_mailer.rb
+++ web/app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: Rails.application.credentials.gmail[:user_name]
   def reset_password_email(user)
     @user = User.find(user.id)
-    @url = "#{Settings.site.top_url}users_password_edit?token=#{user&.reset_password_token}"
+    @url = "https://middlemanagement.app.magia.runteq.jp/users_password_edit?token=#{user&.reset_password_token}"
     mail(to: user.email, subject: '【MiddleManagement】パスワード再設定')
   end
 end
